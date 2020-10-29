fdd872595863965e5b61400b3aa9fddb9b691b46
Arthur Strutzenberg <astrutzenberg@godaddy.com>
2020-10-29 16:47:26 -0700

    making the upstream endpoint a config option, and setting it to the plesk catalog CDN

---
+++ ApsCache/ConfigOptions/ApsSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApsCache.ConfigOptions
+{
+
+    public interface IApsSettings
+    {
+        public string Endpoint { get; set; }
+    }
+    public class ApsSettings:IApsSettings
+    {
+        public const string ApsConfigSectionName = "APSSettings";
+
+        public string Endpoint { get; set; }
+    }
+}
--- ApsCache/Startup.cs
+++ ApsCache/Startup.cs
@@ -2,20 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApsCache.ConfigOptions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 namespace ApsCache
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ApsSettings>(_configuration.GetSection(ApsSettings.ApsConfigSectionName));
+
+            //needed to avoid an issue https://github.com/dotnet/aspnetcore/issues/8302
+            // mostly to translate the body of the inbound request to the body of the upstream request
+            services.Configure<IISServerOptions>(options => { options.AllowSynchronousIO = true; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
